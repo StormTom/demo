9fdb5a5acfee9f5a129dc75153eb217308fd6a28
Tom Hennessy <tom.hennessy@storm12.co.uk>
2018-09-19 10:27:38 +0100

    base content
    this is the first content put into the repository.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<div>
+	<h1>This is a demonstration of how git repos work</h1>
+	<h3>Introduction</h3>
+	<p>This will give you a run through of git and hopefully allow you to understand how it works :)</p>
+	<img src="imgs/pic3.png">
+	<p>This is the git desktop gui (you can use any other gui and all that will change is the look of it really) which ill use for this demo.</p>
+	<br/><img src="imgs/pic1.png">
+	<p>clicking this will let us choose the branch we are currently working in.</p>
+	<br/><img src="imgs/pic4.png">
+	<p>This is the dropdown you get when clicking it. As you can see there is currently just the master branch. This is the 'live' version of the site.</p>
+	<br/><img src="imgs/pic5.png">
+	<p>And this is where you create your new branch to then work on a feature etc.</p>
+</div>
